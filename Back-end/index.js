b7f9bd65adb0f4b7252766eabde52f0eb0339748
Rajesh Manoharan <94797806+RajeshManoharan92@users.noreply.github.com>
2022-09-06 18:12:17 +0530

    Delete Back-end directory

--- Back-end/index.js
+++
@@ -1,9 +0,0 @@
-
-const http = require("http");
-const app = require("./module/module");
-const server = http.createServer(app)
-
-
-server.listen(process.env.PORT||3002, () => {
-    console.log(`Server running on port ${3002}`);
-  });
