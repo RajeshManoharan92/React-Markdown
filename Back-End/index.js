b39418c3df00435e977a00ae8ea02da998e64210
Rajesh Manoharan <94797806+RajeshManoharan92@users.noreply.github.com>
2022-09-14 22:11:43 +0530

    Add files via upload

---
+++ Back-End/index.js
@@ -0,0 +1,21 @@
+const mongo = require("./shared")
+const express = require('express');
+require('dotenv').config()
+const cors = require('cors')
+const router = require("./module/module")
+
+
+mongo.connect();
+
+var app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use("/",router)
+
+app.listen(process.env.PORT||3002, () => {
+  console.log(`Server running on port ${3002}`);
+})
+
